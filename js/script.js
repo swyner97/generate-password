f5f296777b043bb8d551172c55d5492ef50de29b
swyner97 <swyner97@gmail.com>
2022-07-23 17:06:45 -0700

    to lower and upper case functs

--- js/script.js
+++ js/script.js
@@ -1,13 +1,45 @@
 // Assignment code here
 let resultsEl = document.getElementById('#password');
-let lengthEl = document.getElementById('#output').innerHTML;
+let lengthEl = document.getElementById('#length');
 let uppercaseEl = document.getElementById('#uppercase');
 let lowercaseEl = document.getElementById('#lowercase');
 let numericEl = document.getElementById('#numeric');
 let specialCharEl = document.getElementById('#special-chars');
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-console.log(lengthEl);
+// const randoFunc = {
+//   lower: getRandoLower,
+//   upper: getRandoUpper,
+//   number: getRandoNum,
+//   symbol: getRandoSym
+// }
+
+
+// get random uppercase letter 
+
+// getRandoUpper = () => {
+//   let randoUpper = alphabet[Math.floor(Math.random() * alphabet.length)]
+//   console.log(randoUpper)
+// }
+
+// getRandoUpper();
+
+getRandoLower = () => {
+  let randoLower = alphabet[Math.floor(Math.random() * alphabet.length)].toLowerCase()
+  console.log(randoLower)
+}
+
+getRandoLower();
+
+
+
+
+
+
+generatePassword = () => {
+
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -16,10 +48,14 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
+
 }
+
+writePassword();
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
